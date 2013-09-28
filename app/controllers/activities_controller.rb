0bdceb262d70e99dfc0c9143cb45a8700cce19bd
Chris Makoumbou <cmakoumbou@gmail.com>
2013-09-28 18:52:29 +0100

    Activities small fix

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,6 +4,6 @@ class ActivitiesController < ApplicationController
   def index
     #@comment_activities = PublicActivity::Activity.order("created_at desc").where(recipient_id: current_user.textpost_ids, recipient_type: "Textpost")
     #@relationship_activities = PublicActivity::Activity.order("created_at desc").where(recipient_id: current_user, recipient_type: "User")
-    @activities = PublicActivity::Activity.order("created_at desc").where(recipient_id: current_user, recipient_type: "User")
+    @activities = PublicActivity::Activity.order("created_at desc").where(recipient_id: current_user.id, recipient_type: "User").where.not(owner_id: current_user.id)
   end
 end
