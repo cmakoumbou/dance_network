585425389a21cc5d8f655b30ba79fe145e116800
Chris Makoumbou <cmakoumbou@gmail.com>
2014-08-06 05:37:17 +0100

    End of redesign

--- config/routes.rb
+++ config/routes.rb
@@ -28,8 +28,4 @@ DanceNetwork::Application.routes.draw do
   get '/users/:id/following', to: 'users#following', as: :following_user
   get '/users/:id/followers', to: 'users#followers', as: :followers_user
   get '/activities', to: 'activities#index', as: :activities_index
-
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
 end
